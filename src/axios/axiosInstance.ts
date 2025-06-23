94b068d07928f6d558e30c903e6bc9a732e6915f
ibrohim-001 <rustamovibrohimjon62@hmail.com>
2025-06-23 14:39:01 +0500

    tg web app 3

--- src/axios/axiosInstance.ts
+++ src/axios/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://your-backend-domain.com", // Change to your API base
+  baseURL: "https://127.0.0.1:8000",
 });
 
 axiosInstance.interceptors.request.use((config) => {
